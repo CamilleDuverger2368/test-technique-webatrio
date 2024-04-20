7b99445b860bdc841e8f54df30b1f3a39c68f859
grandma2368 <46317358+grandma2368@users.noreply.github.com>
2024-04-20 16:16:46 +0200

    test-technique-webatrio : API DOC and BDD

--- README.md
+++ README.md
@@ -23,3 +23,61 @@ Les actions suivantes ont été réalisées dans l'odre énoncé :
 --> implémentation du endpoint de listing d'emplois entre deux dates pour un utilisateur spécifique
 
 Je suis certaine qu'il existe un système de jointure de table qui permet de faire plus proprement et en utilisant moins de ressources que ce que j'ai codé pour récupérer les données des endpoints 3, 4 et 5 de l'exercice, mais j'ai préféré aller au plus court pour rester dans le temps imparti, quitte à devoir refactoriser le code plus tard quand le temps le permettra.
+N'ayant plus le temps de générer une API DOC avec un swagger, je termine ce readme avec les informations nécessaires à l'utilisation de cette API.
+
+
+# API
+
+Les endpoints suivants nécessitent d'être précédés par la base de votre URL.
+
+## /api/user/register
+
+Create a new user
+
+Méthode : POST
+Mandatories parameters : 
+--> name : user's name (string format)
+--> firstName : user's first name (string format)
+--> birthdate : user's birthdate (date format)
+Returns : success = null / error = 400 and error's message
+
+## /api/user/{id}/add/job
+
+Add job to user
+
+Méthode : POST
+Mandatories parameters : 
+--> id : user's id (int format)
+--> position : user's position in job (string format)
+--> entreprise : job's entreprise (string format)
+--> startingDate : job's start (date format)
+Non mandatories parameters : 
+--> endingDate : job's end (date format)
+Returns : success = "user has a new experience" / error = 400 and error's message
+
+## /api/user/
+
+List all users and their actual jobs
+
+Méthode : GET
+Returns : success = list of users and their jobs / error = 400 and error's message
+
+## /api/user/entreprise/{name}
+
+List all employees of an entreprise
+
+Méthode : GET
+Mandatories parameters : 
+--> name : entreprise's name (string format)
+Returns : success = list of employees of this entreprise / error = 400 and error's message
+
+## /api/user/{id}/get/jobs?from=&to=
+
+List all user's jobs between two dates
+
+Méthode : GET
+Mandatories parameters : 
+--> id : user's id (int format)
+--> from : begin of the search (date format)
+--> to : end of the search (date format)
+Returns : success = array of jobs / error = 400 and error's message
---
+++ test-bdd-webatrio.sql
@@ -0,0 +1,109 @@
+-- MySQL dump 10.13  Distrib 8.0.32, for Win64 (x86_64)
+--
+-- Host: localhost    Database: webatrio
+-- ------------------------------------------------------
+-- Server version	8.0.32
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `entreprise`
+--
+
+DROP TABLE IF EXISTS `entreprise`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `entreprise` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `entreprise`
+--
+
+LOCK TABLES `entreprise` WRITE;
+/*!40000 ALTER TABLE `entreprise` DISABLE KEYS */;
+INSERT INTO `entreprise` VALUES (2,'entreprise-test'),(3,'entreprise-testa');
+/*!40000 ALTER TABLE `entreprise` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `job`
+--
+
+DROP TABLE IF EXISTS `job`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `job` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `entreprise_id` int NOT NULL,
+  `employee_id` int NOT NULL,
+  `position` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `starting_date` date NOT NULL,
+  `ending_date` date DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `IDX_FBD8E0F8A4AEAFEA` (`entreprise_id`),
+  KEY `IDX_FBD8E0F88C03F15C` (`employee_id`),
+  CONSTRAINT `FK_FBD8E0F88C03F15C` FOREIGN KEY (`employee_id`) REFERENCES `user` (`id`),
+  CONSTRAINT `FK_FBD8E0F8A4AEAFEA` FOREIGN KEY (`entreprise_id`) REFERENCES `entreprise` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `job`
+--
+
+LOCK TABLES `job` WRITE;
+/*!40000 ALTER TABLE `job` DISABLE KEYS */;
+INSERT INTO `job` VALUES (1,2,1,'test-job','1998-01-20','1998-01-22'),(2,2,1,'test-job','1998-01-20',NULL),(3,3,2,'test-job-a','1998-01-20',NULL),(4,2,2,'test-job-t','2020-01-20','2024-01-20');
+/*!40000 ALTER TABLE `job` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `first_name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `birthdate` date NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (1,'TEST','test','1998-01-22'),(2,'TESTa','testa','1968-01-22'),(3,'TESTb','testb','1950-12-02');
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-04-20 16:16:08
